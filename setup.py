07606698e47db9133ba84630dc36d7dcde568afb
callumski <callum@cmdatasol.com>
2018-12-05 00:08:40 +0100

    set up files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='movit',
+    version='0.1',
+    packages=['movit', ],
+    author='Callum MacGregor',
+    include_package_data=True,
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[]
+)
