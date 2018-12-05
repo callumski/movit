7ef007fcc65601d15252ee4617c798b968a0866e
callumski <callum@cmdatasol.com>
2018-12-05 21:31:49 +0100

    Added tests for exit moves

--- tests/test_movit.py
+++ tests/test_movit.py
@@ -1,10 +1,7 @@
 from movit.movit import Board, Move, Piece
 
-BOARD_1 = '[["X"]]'
-BOARD_2 = '[["X","X","X"],["X","a","X"],["X","X","X"]]'
-BOARD_3 = '[["X","X","X","X"],["X","b","a","X"],["X","X","X","X"]]'
-BOARD_4 = '[["X","X","X","X"],["X","a"," ","X"],["X","X","X","X"]]'
-BOARD_5 = '[["X","X","X","X","X"],["X","a"," ","X"],["X"," "," ","X"],["X","X","X","X","X"]]'
+from tests.conftest import BOARD_1, BOARD_2, BOARD_3, BOARD_4, BOARD_5, BOARD_6, \
+    BOARD_7
 
 
 def test_board_init():
@@ -44,24 +41,45 @@ def test_board_is_move_available_success():
     m = Move(p, 1, 0)
     assert b.is_move_available(m)
 
+    b = Board(BOARD_5)
+    p = b.get_piece('a')
+    m = Move(p, 1, 0)
+    assert b.is_move_available(m)
+
 
 def test_board_is_move_available_failure():
     b = Board(BOARD_4)
     p = b.get_piece('a')
     m = Move(p, 0, 1)
     assert not b.is_move_available(m)
-    m = Move(p, 1, 1)
+    m = Move(p, -1, 0)
+    assert not b.is_move_available(m)
+
+
+def test_board_is_exit_move_available_success():
+    b = Board(BOARD_6)
+    p = b.get_piece('b')
+    m = Move(p, 0, 1)
+    assert b.is_move_available(m)
+
+
+def test_board_is_exit_move_available_failure():
+    b = Board(BOARD_7)
+    p = b.get_piece('b')
+    m = Move(p, 0, 1)
     assert not b.is_move_available(m)
 
 
 def test_board_get_available_moves():
     b = Board(BOARD_5)
-    p = b.get_piece('a')
+    a = b.get_piece('a')
+    c = b.get_piece('c')
     moves = b.get_available_moves()
-    assert len(moves) == 2
-    print(moves)
-    assert Move(p, 1, 0) in moves
-    assert Move(p, 0, 1) in moves
+    assert len(moves) == 4
+    assert Move(a, 1, 0) in moves
+    assert Move(a, 0, 1) in moves
+    assert Move(c, -1, 0) in moves
+    assert Move(c, 0, -1) in moves
 
 
 def test_move_get_next_position():
